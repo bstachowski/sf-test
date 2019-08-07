db9ea492e088b9d010ec9df71fb118b321743e5e
bst <bstachowski94@gmail.com>
2019-08-07 16:19:46 +0200

    do: use db data

--- web/src/Controller/DefaultController.php
+++ web/src/Controller/DefaultController.php
@@ -13,7 +13,8 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        $blehs = ['ble1','ble2'];
+//        $blehs = ['ble1','ble2'];
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'blehs' => $blehs,
