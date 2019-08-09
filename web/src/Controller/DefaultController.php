3a5588826fd1d049276a1775faff6e1e1b0e5f00
bst <bstachowski94@gmail.com>
2019-08-09 16:37:04 +0200

    3.26.

--- web/src/Controller/DefaultController.php
+++ web/src/Controller/DefaultController.php
@@ -5,16 +5,23 @@ namespace App\Controller;
 use App\Entity\Blehs;
 use App\Services\ItemsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends AbstractController
 {
+    public function __construct($logger) {
+        // logger service
+    }
+
     /**
      * @Route("/default", name="default")
      */
-    public function index(ItemsService $items)
+    public function index(ItemsService $items, Request $request)
     {
 //        $blehs = ['ble1','ble2'];
 
@@ -30,9 +37,11 @@ class DefaultController extends AbstractController
         $entityManager -> persist($blehs2);
         $entityManager -> flush();
 */
-
-
         $blehs = $this -> getDoctrine() -> getRepository(Blehs::class) -> findAll();
+
+        if (!$blehs) {
+            throw $this -> createNotFoundException('Blehs exception');
+        }
 
         $cookie = new Cookie (
             'example_cookie',
@@ -40,16 +49,12 @@ class DefaultController extends AbstractController
             time() + (3600)
         );
 
-
-
-        return $this->render('default/index.html.twig', [
+        $res = new Response();
+        $res = $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'blehs' => $blehs,
             'bleh_items' => $items -> items,
-            ] );
-
-
-        $res = new Response();
+        ] );
         $res -> headers -> setCookie($cookie);
         $res -> send();
 
@@ -63,30 +68,85 @@ class DefaultController extends AbstractController
 //        return $this->redirectToRoute('default2');
     }
 
+//    /**
+//     * @Route("/blog/{page?}", name="blog_list", requirements={"page"="\d+"})
+//     */
+//    public function index2() {
+//
+//        return new Response("Route 2");
+//
+//    }
+
+//    /**
+//     * @Route("/articles/{locale}/{year}/{slug}/{category}",
+//     *     defaults={"category":"computers"},
+//     *     name="blog_list",
+//     *     requirements={
+//         *     "locale":"en|fr",
+//         *     "page"="\d+",
+//         *     "category":"computers|rtv",
+//         *     "year":"\d+" }
+//     *     )
+//     */
+//    public function index3() {
+//
+//        return new Response("Route 3");
+//
+//    }
+
     /**
-     * @Route("/blog/{page?}", name="blog_list", requirements={"page"="\d+"})
-     */
-    public function index2() {
+    * @Route("/generate_url/{param?}", name="generate_url")
+    */
 
-        return new Response("Route 2");
-
+    public function generate_url() {
+        exit($this -> generateUrl(
+            'generate_url',
+            array('param' => 10),
+            UrlGeneratorInterface::ABSOLUTE_URL
+        ));
     }
 
     /**
-     * @Route("/articles/{locale}/{year}/{slug}/{category}",
-     *     defaults={"category":"computers"},
-     *     name="blog_list",
-     *     requirements={
-         *     "locale":"en|fr",
-         *     "page"="\d+",
-         *     "category":"computers|rtv",
-         *     "year":"\d+" }
-     *     )
+     * @Route("/download", name="download")
      */
-    public function index3() {
 
-        return new Response("Route 3");
-
+    public function download() {
+        $path = $this -> getParameter('download_directory');
+        return $this -> file($path.'test.pdf');
     }
 
+    /**
+     * @Route("/redirect_1")
+     */
+
+    public function redirect_1() {
+        return $this -> redirectToRoute('redirect_2', array('param' => 10));
+    }
+
+    /**
+     * @Route("/redirect_2", name="redirect_2")
+     */
+
+    public function redirect_2() {
+        exit('Test redirect_2');
+    }
+
+    /**
+     * @Route("/redirect_controller")
+     */
+
+    public function redirect_controller() {
+        $res = $this -> forward(
+            'App\Controller\DefaultController::redirect_controller_forward_to', array('param' => 1)
+        );
+        return $res;
+    }
+
+    /**
+     * @Route("/redirect_controller_forward_to/{param?}", name="redirect_controller_forward_to")
+     */
+
+    public function redirect_controller_forward_to($param) {
+        exit('Test controller forwarding ' .$param);
+    }
 }
